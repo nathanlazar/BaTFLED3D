35bf591d1d326ac4d9fdf1087b94549eff2db1c2
Nathan Lazar <nathan.lazar@gmail.com>
2017-03-15 17:23:41 -0700

    Fixed and updated kernelize function

--- R/kernelize.R
+++ R/kernelize.R
@@ -1,59 +1,56 @@
-#' Transform a matrix of input data into a matrix of concatenated kernel matrices
+#' Transform a matrix of input data into a matrix of kernel simmilarities values
 #' 
-#' The input matrix should have samples as the rows and features as columns. A kernel will 
-#' computed across all training data for each unique column category. The column names
-#' should begin with the category. E.g. for a cell line input matrix the column names
-#' may be snp.<gene>, meth.<loci>, etc. and this function will produce similarity kernels
-#' for all snp data separately from the methylation data.
+#' The input matrices should have samples as the rows and features as columns. A kernel will 
+#' computed across all samples in the first matrix with respect to the samples in the second matrix.
+#' The two matrices must have the same features. If all features are binary 0,1, then
+#' the Jaccard similarity kernel will be used, otherwise, a Gaussian kernel with standard deviation
+#' equal to s times the mean distances between samples.
 #' 
 #' @importFrom stats dist
 #' 
 #' @export
-#' @param m matrix on which to compute kernels
-#' @param sigma2 numeric bandwidth (variance) for Gaussian kernels (default: 100)
+#' @param m1 matrix on samples X features to compute kernels on 
+#' @param m2 matrix of samples X features to compute kernels with respect to.
+#' @param s numeric multiplier of standard deviation for the Gaussian kernels (default:1).
 #' 
-#' @return m.kern matrix with the kernels concatenated column-wise
+#' @return matrix of similarities between rows of m1 and rows of m2.
 #' 
 #' @examples
-#' m <- matrix(rnorm(800), 8, 100, dimnames=list(paste0('sample.', 1:8), 
-#'                           c(paste0('rna.', 1:20), paste0('meth.', 1:30), paste0('mut.', 1:50))))
-#' m[,51:100] <- rbinom(8*50, 1, 0.5)
-#' m.kern <- kernelize(m)
+#' m1 <- matrix(rnorm(200), 8, 25, dimnames=list(paste0('sample.', 1:8), paste0('feat.', 1:25)))
+#' m2 <- matrix(rnorm(100), 4, 25, dimnames=list(paste0('sample.', 9:12), paste0('feat.', 1:25)))
+#' kernelize(m1, m1)
+#' kernelize(m1, m1, s=.5)
+#' kernelize(m2, m1)
 
-kernelize <- function(m, sigma2=100) {
-  if(ncol(m)==0) return(m)
+kernelize <- function(m1, m2=NA, s=1) {
+  if(missing(m2)) m2 <- m1
+
+  if(!all.equal(colnames(m1), colnames(m2))) 
+    stop('The columns must match between the two matrices')
   
-  col.names <- colnames(m)
-  cat <- unique(sapply(strsplit(col.names, '.', fixed=T), '[', 1))
-  kerns <- list()
-  
-  for(i in 1:length(cat)) {
-    sub <- m[,grepl(paste0('^', cat[[i]]), colnames(m))]
-    # If the submatrix is binary, compute Jaccard kernel
-    # otherwise compute the Gaussian kernel Pearson correlations
-    if(min(sub, na.rm=T)==0 & max(sub, na.rm=T)==1 & length(unique(as.vector(sub)))==2) {
-      # compute Jaccard kernel
-      kerns[[i]] <- matrix(NA, nrow(m), nrow(m), dimnames=list(row.names(m),
-                                               paste0(cat[[i]], '.', rownames(m))))
-      for(j in 1:nrow(m)) for (k in 1:nrow(m)) {
-        if(j == k) {
-          kerns[[i]][j,k] <- 1 
-        } else {
-          union <- sum(sub[j,] | sub[k,])
-          if(union != 0) {
-            kerns[[i]][j,k] <- sum(sub[j,] & sub[k,])/union
-          } else kerns[[i]][j,k] <- 0
-        }
-      }
+  # If the matrices are binary, compute Jaccard kernel
+  if(min(m1, na.rm=T)==0 && max(m1, na.rm=T)==1 && length(unique(as.vector(m1)))==2 &&
+     min(m2, na.rm=T)==0 && max(m2, na.rm=T)==1 && length(unique(as.vector(m2)))==2) {
+    # compute Jaccard kernel
+    if (identical(m1, m2) == TRUE) {
+      K <- 1 - as.matrix(dist(m1, method='binary'))
     } else {
-      kerns[[i]] <- exp(as.matrix(-(dist(sub, diag=T, upper=T)^2)/sigma2))
-        #cor(t(sub))
-      colnames(kerns[[i]]) <- paste0(cat[[i]], '.', rownames(m))
+      K <- 1 - as.matrix(dist(rbind(m1, m2), method='binary'))
+      K <- K[1:nrow(m1), (nrow(m1) + 1):(nrow(m1) + nrow(m2))]
     }
-  }
-  # Combine the kernels
-  m.kern <- matrix(NA, nrow(m), 0, dimnames=list(rownames(m), c()))
-  for(i in 1:length(kerns))
-    m.kern <- cbind(m.kern, kerns[[i]])
-  return(m.kern)
+  } else {
+    # Compute Gaussian kernel
+    if (identical(m1, m2) == TRUE) {
+      D <- as.matrix(dist(m1))
+    } else {
+      D <- as.matrix(dist(rbind(m1, m2)))
+      D <- D[1:nrow(m1), (nrow(m1) + 1):(nrow(m1) + nrow(m2))]
+    }
+    d_mean <- mean(as.matrix(dist(m2)))
+    sigma2 <- (s * d_mean)^2
+    K <- exp(-D^2/(2*sigma2))
+  }    
+  rownames(K) <- rownames(m1)
+  colnames(K) <- rownames(m2)
+  return(K)
 }
